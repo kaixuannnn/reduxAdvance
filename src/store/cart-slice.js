971b522459789f5e19e9c62eb873c777ae084f9e
Kai Xuan <kaixuan@agmostudio.com>
2021-07-28 21:40:16 +0800

    action creator thunk

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -1,5 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit'
-
+import { uiAction } from './ui-slide'
 const cartSlice = createSlice({
   name: 'cart',
   initialState: { items: [], totalQuantity: 0 },
@@ -34,6 +34,49 @@ const cartSlice = createSlice({
     },
   },
 })
+
+export const sendCartData = (cart) => {
+  return async (dispatch) => {
+    dispatch(
+      uiAction.showNotification({
+        status: 'pending',
+        title: 'Sending...',
+        message: 'Sending cart data',
+      })
+    )
+
+    const sendRequest = async () => {
+      const response = await fetch(
+        'https://loyal-symbol-295908-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json',
+        { method: 'PUT', body: JSON.stringify(cart) }
+      )
+
+      if (!response.ok) {
+        throw new Error('Sending cart data failed.')
+      }
+    }
+
+    try {
+      await sendRequest()
+      dispatch(
+        uiAction.showNotification({
+          status: 'success',
+          title: 'Success!',
+          message: 'Sent cart data successfully!',
+        })
+      )
+    } catch (error) {
+      dispatch(
+        uiAction.showNotification({
+          status: 'error',
+          title: 'Error!',
+          message: 'Sending cart data failed',
+        })
+      )
+    }
+  }
+}
+
 export const cartAction = cartSlice.actions
 
 export default cartSlice
